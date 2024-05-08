dd9b9b452f2d674b31468ae3ba1647f3c6e75c41
Adriel Agusti Gandulfo Millan <adrielagustigandulfomillan@Adriels-MacBook-Pro.local>
2024-05-08 16:40:39 -0300

    Server.js change

--- srv/server.js
+++ srv/server.js
@@ -1,4 +1,26 @@
+// const cds = require("@sap/cds");
+// const cov2ap = require("@cap-js-community/odata-v2-adapter");
+// cds.on("bootstrap", (app) => app.use(cov2ap()));
+// module.exports = cds.server;
+
+
+const express = require("express");
 const cds = require("@sap/cds");
-const cov2ap = require("@cap-js-community/odata-v2-adapter");
-cds.on("bootstrap", (app) => app.use(cov2ap()));
-module.exports = cds.server;
+const proxy = require("@sap/cds-odata-v2-adapter-proxy");
+
+const host = "0.0.0.0";
+const port = process.env.PORT || 4004;
+
+(async () => {
+  const app = express();
+
+  // OData V2
+  app.use(proxy());
+
+  // OData V4
+  await cds.connect.to("db");
+  await cds.serve("all").in(app);
+
+  const server = app.listen(port, host, () => console.info(`app is listing at ${host}:${port}`));
+  server.on("error", error => console.error(error.stack));
+})();
