eb5a943ef9c87a50769ca61d34654ed339602a77
Adriel Agusti Gandulfo Millan <adrielagustigandulfomillan@Adriels-MacBook-Pro.local>
2024-05-08 17:16:56 -0300

    fetch added

--- srv/server.js
+++ srv/server.js
@@ -1,3 +1,4 @@
+import fetch from "node-fetch";
 // const cds = require("@sap/cds");
 // const cov2ap = require("@cap-js-community/odata-v2-adapter");
 // cds.on("bootstrap", (app) => app.use(cov2ap()));
