7508072594d45dd65a0cb2015701b93761fe605d
Adriel Agusti Gandulfo Millan <adrielagustigandulfomillan@Adriels-MacBook-Pro.local>
2024-05-08 15:18:45 -0300

    proxied2

--- srv/server.js
+++ srv/server.js
@@ -2,5 +2,5 @@ const cds = require("@sap/cds");
 const cov2ap = require("@cap-js-community/odata-v2-adapter");
 cds.on("bootstrap", (app) => { 
         app.use(cov2ap())
-        app.proxy({ path: 'v2', port:8080 , host:'0.0.0.0' }) });
+        app.use(proxy({ path: 'v2', port:8080 , host:'0.0.0.0' }))});
 module.exports = cds.server;
