43b55a20fa8f83a927a4d6559eaa8d13be48a64d
Adriel Agusti Gandulfo Millan <adrielagustigandulfomillan@Adriels-MacBook-Pro.local>
2024-05-08 16:44:23 -0300

    Service.js change

--- srv/server.js
+++ srv/server.js
@@ -4,23 +4,23 @@
 // module.exports = cds.server;
 
 
-const express = require("express");
-const cds = require("@sap/cds");
-const proxy = require("@sap/cds-odata-v2-adapter-proxy");
+// const express = require("express");
+// const cds = require("@sap/cds");
+// const proxy = require("@sap/cds-odata-v2-adapter-proxy");
 
-const host = "0.0.0.0";
-const port = process.env.PORT || 4004;
+// const host = "0.0.0.0";
+// const port = process.env.PORT || 4004;
 
-(async () => {
-  const app = express();
+// (async () => {
+//   const app = express();
 
-  // OData V2
-  app.use(proxy());
+//   // OData V2
+//   app.use(proxy());
 
-  // OData V4
-  await cds.connect.to("db");
-  await cds.serve("all").in(app);
+//   // OData V4
+//   await cds.connect.to("db");
+//   await cds.serve("all").in(app);
 
-  const server = app.listen(port, host, () => console.info(`app is listing at ${host}:${port}`));
-  server.on("error", error => console.error(error.stack));
-})();
+//   const server = app.listen(port, host, () => console.info(`app is listing at ${host}:${port}`));
+//   server.on("error", error => console.error(error.stack));
+// })();
